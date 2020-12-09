ad3682b9ea06fff2f795f0f051ba13faf241784f
Dhiraj Kale <67183746+Dvkale17@users.noreply.github.com>
2020-12-09 19:36:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,url_for,request
+import pandas as pd
+import pickle
+import jsonify
+import requests
+from sklearn.preprocessing import StandardScaler
+# load the model from disk
+loaded_model=pickle.load(open('transform_model.pkl', 'rb'))
+cv=pickle.load(open('tf-idf-tranform.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+standard_to = StandardScaler()
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message =request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction =loaded_model.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
